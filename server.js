690d6b26433031ca925fd53dd3da6add038d5768
Micah Sudweeks <micahsudweeks@gmail.com>
2023-01-27 13:47:52 -0700

    Created an interface on personal server

--- server.js
+++ server.js
@@ -10,16 +10,29 @@ const Redis = require ('redis');
 
 const redisClient = Redis.createClient({url:"redis://127.0.0.1:6379"});
 
+const {v4: uuidv4} = require('uuid');
+
 app.use(bodyParser.json()); //This looks for incoming data
+
+app.use(express.static('public'));
 
 app.get("/", (req, res)=> {
     res.send("Hello Micah");
 });
 
-app.post('/login', (req, res) =>{
+app.post('/login', async(req, res) =>{
     const loginUser = req.body.userName;
+    const loginPassword = req.body.password;
+    console.log(req.body);
     console.log('Login username: '+loginUser);
-    res.send('Hello '+loginUser);
+    const correctPassword = await redisClient.hGet('UserMap', loginUser);
+    if (loginPassword==correctPassword){
+        const loginToken = uuidv4();
+        res.send(loginToken);
+    } else {
+        res.status(401);
+        res.send('Incorrect password for ' +loginUser);
+    }
 });
 
 app.listen(port, () =>{
