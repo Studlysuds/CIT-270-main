e596000c5ba8beb6b3dd0754e1c4018b227783c2
Micah Sudweeks <micahsudweeks@gmail.com>
2023-02-27 13:41:16 -0700

    Changed the key, cert, and ca to one that we can use more readily

--- server.js
+++ server.js
@@ -71,9 +71,9 @@ app.post('/login', async(req, res) =>{
 // levi was here
 https.createServer(
     {
-        key: fs.readFileSync('./server.key'),
-        cert: fs.readFileSync('./server.cert'),
-        ca:fs.readFileSync('./chain.pem')
+        key: fs.readFileSync('/etc/letsencrypt/live/micahsudweeks.cit270.com/privkey.pem'),
+        cert: fs.readFileSync('/etc/letsencrypt/live/micahsudweeks.cit270.com/cert.pem'),
+        ca: fs.readFileSync('/etc/letsencrypt/live/micahsudweeks.cit270.com/fullchain.pem')
     },
     app
 ).listen(port, ()=>{
